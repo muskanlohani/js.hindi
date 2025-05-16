a715c2bf072b55daaa10eecd8a1d7a8543c579a3
muskanlohani <muskanlohani7@gmail.com>
2025-05-16 06:34:56 +0000

    variables

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = "678"
+let accountEmail="muska@gmail.com"
+var passsword="67"
+accountcity='delhi'
+let accountstate;
+// accountId ="989"
+// console.log(accountId);
+accountEmail="sanj@gmail.com"
+passsword="789"
+console.table([accountId,accountEmail,passsword,accountcity,accountstate])
+/*muskan */
