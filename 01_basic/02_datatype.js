a8e659527836f1d5b107ecd5d687fbf0e2979baf
muskanlohani <muskanlohani7@gmail.com>
2025-05-16 07:27:41 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,14 @@
+console.log("m")
+ console.log("o");
+
+// alert("j")
+"user strict"
+let name ="muskan"//string datatype
+console.log(typeof 4)
+console.log(typeof 908779367489500)
+console.log(typeof  "muskan" )
+console.log(typeof true);
+console.log(typeof "")
+console.log(typeof  null)
+
+
