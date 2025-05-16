fe7f48623da6591da7b72195b05334d1a0235af7
muskanlohani <muskanlohani7@gmail.com>
2025-05-16 09:34:55 +0000

    conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,27 @@
+// let score="muskan";
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// console.log(valueInNumber);
+
+
+/*
+ "33"=>33
+ "33abe"=> NaN
+ true=>1
+ false=>0
+*/
+
+// let isLoggedIn="m"
+// let booleanIsloggedIn =Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
+/*
+1=>true 0=>false   ""=>false  "muskan"=>true
+
+*/ 
+let num=33
+let stringnum=String(num)
+console.log(stringnum)
+console.log(typeof stringnum)
